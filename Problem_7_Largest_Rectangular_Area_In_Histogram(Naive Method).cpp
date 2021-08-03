9194e2a42ec608bfd9c0d24ec20e6daad37fb16a
SachinSarin <60125906+SachinSarin@users.noreply.github.com>
2021-08-03 18:42:18 +0530

    Very Imp Problem O(N)

---
+++ Problem_7_Largest_Rectangular_Area_In_Histogram(Naive Method).cpp
@@ -0,0 +1,58 @@
+/*
+Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.
+
+ 
+
+Example 1:
+
+
+Input: heights = [2,1,5,6,2,3]
+Output: 10
+Explanation: The above is a histogram where width of each bar is 1.
+The largest rectangle is shown in the red area, which has an area = 10 units.
+Example 2:
+
+
+Input: heights = [2,4]
+Output: 4
+ 
+
+Constraints:
+
+1 <= heights.length <= 105
+0 <= heights[i] <= 104
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int getMaxArea(int arr[],int n){
+    stack <int> s;
+    int res=0;
+    int tp;
+    int curr;
+    
+    for(int i=0;i<n;i++){
+        while(s.empty()==false && arr[s.top()]>=arr[i]){
+            tp=s.top();s.pop();
+            curr=arr[tp]* (s.empty() ? i : i - s.top() - 1);
+            res=max(res,curr);
+        }
+        s.push(i);
+    }
+    while(s.empty()==false){
+        tp=s.top();s.pop();
+        curr=arr[tp]* (s.empty() ? n : n - s.top() - 1);
+        res=max(res,curr);
+    }
+    
+    return res;
+    
+}
+
+int main() 
+{ 
+    int arr[]={6,2,5,4,1,5,6};
+    int n=7;
+    cout<<"Maximum Area: "<<getMaxArea(arr,n);
+    return 0; 
+}
